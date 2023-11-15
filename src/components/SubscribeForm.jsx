d32785ceb5184e068fd090b13d8f87b31003cdb4
Moktadir Rahman <autocircled@gmail.com>
2023-11-15 12:43:07 +0600

    subscribe form ok

---
+++ src/components/SubscribeForm.jsx
@@ -0,0 +1,27 @@
+import '../assets/css/form.css'
+
+const SubscribeForm = () => {
+    return (
+        <section className="subscribe-form bg-white py-5">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-5 text-center">
+                        <div className="form-header">
+                            <h3 className="title d-block fw-bold text-uppercase">SUBSCRIBE</h3>
+                            <h3 className="description text-capitalize">Subscribe to get the latest news about us</h3>
+                            <p className='info small text-capitalize'>Please drop your email below to get daily update about what we do</p>
+                        </div>
+                        <div className="form-body">
+                            <div className="input-group-manual">
+                                <input type="text" className="form-control" placeholder="Enter your email" />
+                                <button className="btn btn-warning text-white">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SubscribeForm
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import AppNavBar from '../components/AppNavBar'
 import FeaturedProjects from '../components/FeaturedProjects'
 import Hero from '../components/Hero'
+import SubscribeForm from '../components/SubscribeForm'
 import WorkList from '../components/WorkList'
 
 const HomePage = () => {
@@ -10,6 +11,7 @@ const HomePage = () => {
             <Hero />
             <WorkList />
             <FeaturedProjects />
+            <SubscribeForm />
 
 
         </>
