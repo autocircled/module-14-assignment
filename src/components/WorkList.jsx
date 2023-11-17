70b487099dc0745b1654c23e019ed25da16441df
Moktadir Rahman <autocircled@gmail.com>
2023-11-17 12:23:05 +0600

    worklist done

--- src/components/WorkList.jsx
+++ src/components/WorkList.jsx
@@ -1,33 +1,23 @@
 import '../assets/css/work-list.css'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import { MdOutlineShowChart } from "react-icons/md";
-import { LuHeartHandshake } from "react-icons/lu";
-import { GiSuitcase } from "react-icons/gi";
-import { IoMdArrowForward } from "react-icons/io";
+import { useEffect, useState } from 'react'
 import Counter from './Counter';
+import { getJsonData } from '../apiRequests/apiRequest';
 
 const WorkList = () => {
-    const [data] = useState([
-        {
-            title: 'Grow Your Business',
-            description: 'We help identify the best ways to improve your business',
-            icon: MdOutlineShowChart,
-            link: '/grow'
-        },
-        {
-            title: 'Improve brand loyalty',
-            description: 'We help identify the best ways to improve your business',
-            icon: LuHeartHandshake,
-            link: '/improve'
-        },
-        {
-            title: 'Grow Your Business',
-            description: 'We help identify the best ways to improve your business',
-            icon: GiSuitcase,
-            link: '/business'
-        },
-    ])
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await getJsonData('work-list.json');
+                setData(data['list']);
+            } catch (error) {
+                console.log(error);
+            }
+        })()
+    }, [])
+
     return (
         <section className="work-list py-5">
             <div className="container">
@@ -42,14 +32,14 @@ const WorkList = () => {
 
                         <div key={index.toString()} className="col-md-4">
                             <div className="work-icon">
-                                <span className='d-inline-block'><item.icon /></span>
+                                <span className='d-inline-block'><i className={item.icon}></i></span>
                             </div>
                             <div className="work-body my-5">
                                 <h5 className="title">{item.title}</h5>
                                 <p className="description">{item.description}</p>
 
                             </div>
-                            <Link to={item.link}>Learn More <IoMdArrowForward /></Link>
+                            <Link to={item.link}>Learn More <i className="fas fa-arrow-right"></i></Link>
                         </div>
                     ))}
                 </div>
