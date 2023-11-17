c0788bcf32ef1234b289f2621054107a0bb07c24
Moktadir Rahman <autocircled@gmail.com>
2023-11-17 16:32:36 +0600

    counter done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,37 +1,22 @@
-import { FaPeopleGroup } from "react-icons/fa6";
-import { SlLike } from "react-icons/sl";
-import { CiFaceSmile } from "react-icons/ci";
-import { BiSolidObjectsHorizontalCenter } from "react-icons/bi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import '../assets/css/counter.css'
+import { getJsonData } from "../apiRequests/apiRequest";
 
 const Counter = () => {
-    const [data] = useState([
-        {
-            icon: FaPeopleGroup,
-            count: '564651',
-            description: 'Followers',
+    const [data, setData] = useState([])
 
-        },
-        {
-            icon: SlLike,
-            count: '6300',
-            description: 'Solved Problems',
 
-        },
-        {
-            icon: CiFaceSmile,
-            count: '25000',
-            description: 'Happy Customers',
-
-        },
-        {
-            icon: BiSolidObjectsHorizontalCenter,
-            count: '1254',
-            description: 'Projects Completed',
-
-        },
-    ])
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await getJsonData('counter.json');
+                console.log(data);
+                setData(data['data']);
+            } catch (error) {
+                console.log(error);
+            }
+        })()
+    }, [])
     return (
         <section className="counter bg-white py-5">
             <div className="container">
@@ -40,7 +25,7 @@ const Counter = () => {
                         <div key={index.toString()} className="col-6 col-md-3 d-flex flex-column justify-content-start align-items-center mt-5">
                             <div className="counter-inner rounded w-100">
                                 <div className="counter-icon text-center">
-                                    <span className='d-inline-block'><item.icon /></span>
+                                    <span className='d-inline-block'><i className={item.icon}></i></span>
                                 </div>
                                 <div className="counter-body text-center mt-4">
                                     <h3 className="count">{item.count}</h3>
